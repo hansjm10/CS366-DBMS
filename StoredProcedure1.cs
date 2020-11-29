e072445fc7db0445bf647e05a2faf09d49b77af5
piepersc10 <75179249+piepersc10@users.noreply.github.com>
2020-11-29 12:45:52 -0600

    Added files for Stored Procedures

---
+++ StoredProcedure1.cs
@@ -0,0 +1,71 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.Data;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+namespace SQLConnection
+{
+    class StoredProcedure1
+    {
+        MySqlConnection conn = new MySqlConnection();
+        conn.ConnectionString = "server=localhost;user=root;database=employees;port=3306;password=******";
+        MySqlCommand cmd = new MySqlCommand();
+
+        try
+        {
+            Console.WriteLine("Connecting to MySQL...");
+            conn.Open();
+            cmd.Connection = conn;
+            cmd.CommandText = "delimiter $$ drop procedure of exists getUserCred;";
+            cmd.ExecuteNonQuery();
+            cmd.CommandText = "create procedure getUserCred(" +
+                                "INOUT userID varchar(255), INOUT userName varchar(255), OUT userAge)" +
+                                "begin select* into userID, userName, userAge from Users where User_ID = userID;" +
+                                "end $$ delimiter;";
+
+                cmd.ExecuteNonQuery();
+            }
+            catch (MySqlException ex)
+            {
+                Console.WriteLine ("Error " + ex.Number + " has occurred: " + ex.Message);
+            }
+            conn.Close();
+            Console.WriteLine("Connection closed.");
+            try
+            {
+                Console.WriteLine("Connecting to MySQL...");
+                conn.Open();
+                cmd.Connection = conn;
+
+                cmd.CommandText = "add_emp";
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                cmd.Parameters.AddWithValue("@lname", "Jones");
+                cmd.Parameters["@lname"].Direction = ParameterDirection.Input;
+
+                cmd.Parameters.AddWithValue("@fname", "Tom");
+                cmd.Parameters["@fname"].Direction = ParameterDirection.Input;
+
+                cmd.Parameters.AddWithValue("@bday", "1940-06-07");
+                cmd.Parameters["@bday"].Direction = ParameterDirection.Input;
+
+                cmd.Parameters.Add("@empno", MySqlDbType.Int32);
+                cmd.Parameters["@empno"].Direction = ParameterDirection.Output;
+
+                cmd.ExecuteNonQuery();
+
+                Console.WriteLine("Employee number: "+cmd.Parameters["@empno"].Value);
+                Console.WriteLine("Birthday: " + cmd.Parameters["@bday"].Value);
+            }
+            catch (MySql.Data.MySqlClient.MySqlException ex)
+            {
+                Console.WriteLine("Error " + ex.Number + " has occurred: " + ex.Message);
+            }
+            conn.Close();
+            Console.WriteLine("Done.");
+    }
+}
---
+++ StoredProcedure2.cs
@@ -0,0 +1,70 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.Data;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+namespace UsingStoredRoutines
+{
+    class StoredProcedure2
+    {
+        static void Main(string[] args)
+        {
+            MySqlConnection conn = new MySqlConnection();
+            conn.ConnectionString = "server=localhost;user=root;database=employees;port=3306;password=******";
+            MySqlCommand cmd = new MySqlCommand();
+
+            try
+            {
+                cmd.CommandText = "DROP PROCEDURE IF EXISTS add_emp";
+                cmd.ExecuteNonQuery();
+                cmd.CommandText = "CREATE PROCEDURE add_emp(" +
+                                  "IN fname VARCHAR(20), IN lname VARCHAR(20), IN bday DATETIME, OUT empno INT)" +
+                                  "BEGIN INSERT INTO emp(first_name, last_name, birthdate) " +
+                                  "VALUES(fname, lname, DATE(bday)); SET empno = LAST_INSERT_ID(); END";
+                cmd.ExecuteNonQuery();
+            }
+            catch (MySqlException ex)
+            {
+                Console.WriteLine ("Error " + ex.Number + " has occurred: " + ex.Message);
+            }
+            conn.Close();
+            Console.WriteLine("Connection closed.");
+            try
+            {
+                Console.WriteLine("Connecting to MySQL...");
+                conn.Open();
+                cmd.Connection = conn;
+
+                cmd.CommandText = "add_emp";
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                cmd.Parameters.AddWithValue("@lname", "Jones");
+                cmd.Parameters["@lname"].Direction = ParameterDirection.Input;
+
+                cmd.Parameters.AddWithValue("@fname", "Tom");
+                cmd.Parameters["@fname"].Direction = ParameterDirection.Input;
+
+                cmd.Parameters.AddWithValue("@bday", "1940-06-07");
+                cmd.Parameters["@bday"].Direction = ParameterDirection.Input;
+
+                cmd.Parameters.Add("@empno", MySqlDbType.Int32);
+                cmd.Parameters["@empno"].Direction = ParameterDirection.Output;
+
+                cmd.ExecuteNonQuery();
+
+                Console.WriteLine("Employee number: "+cmd.Parameters["@empno"].Value);
+                Console.WriteLine("Birthday: " + cmd.Parameters["@bday"].Value);
+            }
+            catch (MySql.Data.MySqlClient.MySqlException ex)
+            {
+                Console.WriteLine("Error " + ex.Number + " has occurred: " + ex.Message);
+            }
+            conn.Close();
+            Console.WriteLine("Done.");
+        }
+    }
+}
