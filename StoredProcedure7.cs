e3def41c0395346dc5652b5a5a16a0d86f986313
Jordan Hans <jordan.hans@ymail.com>
2020-12-02 13:52:06 -0600

    Updated SP7 and SP8 and added TEST

--- StoredProcedure7.cs
+++ StoredProcedure7.cs
@@ -18,18 +18,39 @@ namespace SQLConnection
 
             try
             {
-                string queryString = "Select Title, Release_Year, Age_Rating, Genre, Developer from Video_Games where Age_Rating != 'M' limit 20";
+                conn.Open();
+                string queryString = "select s.System_Name, avg(r.Score) as avgsc, sum(r.Num_Reviews) as totrevs" + 
+                                     "from (Made_For s inner join Metacritic_Reviews r on Game_ID) inner join Video_Games g on Game_ID" + 
+                                     "where g.Title = title and g.Game_ID = r.Game_ID and g.Game_ID = s.Game_ID and g.Game_ID in (select p.Game_ID from Prefers where p.User_ID in (select u.User_ID from Users U where u.User_ID = userID))";
                 
-                MySqlDataAdapter da = new MySqlDataAdapter();
+                
+                Console.WriteLine("Enter title: ");
+                string userID = Console.ReadLine();
+
+                MySqlDataAdapter da;
+                MySqlCommand command = new MySqlCommand();
+                MySqlParameter param;
                 DataSet ds = new DataSet();
                 DataTable dt = new DataTable();
 
-                da.SelectCommand = new MySqlCommand("Call filterAge17", conn);
-                da.Fill(ds,"Video_Games");
+
+                command.Connection = conn;
+                command.CommandType = CommandType.StoredProcedure;
+                command.CommandText = "showPreferences";
+
+                param = new MySqlParameter("@userID_p", userID);
+
+                param.Direction = ParameterDirection.Input;
+                param.DbType = DbType.String;
+                command.Parameters.Add(param);
+
+                da = new MySqlDataAdapter(command);
+                da.Fill(ds,"Video_Games"); //This will have to change.
                 dt = ds.Tables["Video_Games"];
                 foreach (DataRow dr in dt.Rows)
                 {
-                    Console.WriteLine(dr["Title"] + " " + dr["Release_Year"] + " " + dr["Age_Rating"] + " " + dr["Genre"] + " " + dr["Developer"]);
+                    Console.WriteLine(dr["Title"] + " " + dr["Release_Year"] + " " +
+                    dr["Age_Rating"] + " " + dr["Genre"] + " " + dr["Developer"]);
                 }
             }
             catch (MySql.Data.MySqlClient.MySqlException ex)
--- StoredProcedure8.cs
+++ StoredProcedure8.cs
@@ -17,25 +17,39 @@ namespace SQLConnection
 
             try
             {
+               conn.Open();
                 string queryString = "select s.System_Name, avg(r.Score) as avgsc, sum(r.Num_Reviews) as totrevs" + 
                                      "from (Made_For s inner join Metacritic_Reviews r on Game_ID) inner join Video_Games g on Game_ID" + 
                                      "where g.Title = title and g.Game_ID = r.Game_ID and g.Game_ID = s.Game_ID and g.Game_ID in (select p.Game_ID from Prefers where p.User_ID in (select u.User_ID from Users U where u.User_ID = userID))";
                 
-                string title = "";
-                Console.WriteLine("Enter title: ");
-                title = Console.ReadLine();
+                
+                Console.WriteLine("Enter userID: ");
+                string userID = Console.ReadLine();
 
-                MySqlDataAdapter da = new MySqlDataAdapter();
+                MySqlDataAdapter da;
+                MySqlCommand command = new MySqlCommand();
+                MySqlParameter param;
                 DataSet ds = new DataSet();
                 DataTable dt = new DataTable();
 
-                da.SelectCommand = new MySqlCommand("set @title = " + title + ";");
-                da.SelectCommand = new MySqlCommand("Call showInfo", conn);
+
+                command.Connection = conn;
+                command.CommandType = CommandType.StoredProcedure;
+                command.CommandText = "showInfo";
+
+                param = new MySqlParameter("@userID_p", userID);
+
+                param.Direction = ParameterDirection.Input;
+                param.DbType = DbType.String;
+                command.Parameters.Add(param);
+
+                da = new MySqlDataAdapter(command);
                 da.Fill(ds,"Video_Games"); //This will have to change.
                 dt = ds.Tables["Video_Games"];
                 foreach (DataRow dr in dt.Rows)
                 {
-                    Console.WriteLine(dr["System_Name"] + " " + dr["Avg_Score"] + " " + dr["Total_Reviews"]);
+                    Console.WriteLine(dr["System_Name"] + " " + dr["totrevs"] + " " +
+                    dr["avgrevs"]);
                 }
             }
             catch (MySql.Data.MySqlClient.MySqlException ex)
