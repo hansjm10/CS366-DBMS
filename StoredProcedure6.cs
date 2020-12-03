d5f288758429ec9a47d40b8172bf4933780056c6
piepersc10 <75179249+piepersc10@users.noreply.github.com>
2020-12-03 13:51:00 -0600

    Fixed SP6

--- StoredProcedure6.cs
+++ StoredProcedure6.cs
@@ -19,27 +19,8 @@ namespace SQLConnection
             try
             {
                 conn.Open();
-                Console.WriteLine("Enter system name: ");
-                string systemName = Console.ReadLine();
-                Console.WriteLine("Enter release year 1: ");
-                string rY1 = Console.ReadLine();
-                Console.WriteLine("Enter release year 2: ");
-                string rY2 = Console.ReadLine();
-                Console.WriteLine("Enter age rating: ");
-                string ageRating = Console.ReadLine();
-                Console.WriteLine("Enter genre: ");
-                string gen = Console.ReadLine();
-                Console.WriteLine("Enter developer: ");
-                string dev = Console.ReadLine();
-
                 MySqlDataAdapter da;
                 MySqlCommand command = new MySqlCommand();
-                MySqlParameter param;
-                MySqlParameter param2;
-                MySqlParameter param3;
-                MySqlParameter param4;
-                MySqlParameter param5;
-                MySqlParameter param6;
                 DataSet ds = new DataSet();
                 DataTable dt = new DataTable();
 
@@ -47,37 +28,6 @@ namespace SQLConnection
                 command.Connection = conn;
                 command.CommandType = CommandType.StoredProcedure;
                 command.CommandText = "finalOutput";
-
-                param = new MySqlParameter("@systemName_p", systemName);
-                param2 = new MySqlParameter("@rY1_p", rY1);
-                param3 = new MySqlParameter("@rY2_p", rY2);
-                param4 = new MySqlParameter("@ageRating_p", ageRating);
-                param5 = new MySqlParameter("@gen_p", gen);
-                param6 = new MySqlParameter("@dev_p", dev);
-
-                param.Direction = ParameterDirection.Input;
-                param.DbType = DbType.String;
-                command.Parameters.Add(param);
-
-                param2.Direction = ParameterDirection.Input;
-                param2.DbType = DbType.String;
-                command.Parameters.Add(param2);
-                
-                param3.Direction = ParameterDirection.Input;
-                param3.DbType = DbType.String;
-                command.Parameters.Add(param3);
-                
-                param4.Direction = ParameterDirection.Input;
-                param4.DbType = DbType.String;
-                command.Parameters.Add(param4);
-                
-                param5.Direction = ParameterDirection.Input;
-                param5.DbType = DbType.String;
-                command.Parameters.Add(param5);
-                
-                param6.Direction = ParameterDirection.Input;
-                param6.DbType = DbType.String;
-                command.Parameters.Add(param6);
 
                 da = new MySqlDataAdapter(command);
                 da.Fill(ds,"Video_Games"); 
