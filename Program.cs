45a578f318bf57498a2011153bfa65e951124e51
piepersc10 <75179249+piepersc10@users.noreply.github.com>
2020-12-06 15:54:33 -0600

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -25,48 +25,52 @@ namespace SQLConnection
             string loginSelect = Console.ReadLine();
             string userName, userID, inputAge; 
             int userAge;
-            if (loginSelect == "1"){ //New user registration.
-                Console.WriteLine("Please enter your username, password, and age.");
-                Console.WriteLine("Enter username: ");
-                userName = Console.ReadLine();
-                Console.WriteLine("Enter password: ");
-                userID = Console.ReadLine();
-                Console.WriteLine("Enter age: ");
-                inputAge = Console.ReadLine();
-                Int32.TryParse(inputAge, out userAge);
-                cmd.CommandText = "INSERT INTO Users(User_ID,User_Name,Age) VALUES(?User_ID,?User_Name,?Age)";
-                cmd.Parameters.Add("?User_ID", MySqlDbType.VarChar).Value = userID;
-                cmd.Parameters.Add("?User_Name", MySqlDbType.VarChar).Value = userName;
-                cmd.Parameters.Add("?Age", MySqlDbType.Int32).Value = userAge;
-                cmd.ExecuteNonQuery();
-                Console.WriteLine("Welcome, " + userName + "!");
-            }
-            else if (loginSelect == "2"){ //Login for existing users.
-                Console.WriteLine("Please enter your username and password.");
-                Console.WriteLine("Enter username: ");
-                userName = Console.ReadLine();
-                Console.WriteLine("Enter password: ");
-                userID = Console.ReadLine();
-                Tuple<string, string, string> userCreds = sp1.getUserCred(connString, userID);
-                bool loginCorrect = false;
-                while (loginCorrect == false){
-                    if (userID == userCreds.Item1 && userName == userCreds.Item2){
-                        Console.WriteLine("Welcome back, " + userName + "!");
-                        loginCorrect = true;
-                    }
-                    else{
-                        Console.WriteLine("Login failed. Try again.");
+            bool loggedIn = false;
+            while(loggedIn == false){
+                if (loginSelect == "1"){ //New user registration.
+                    Console.WriteLine("Please enter your username, password, and age.");
+                    Console.WriteLine("Enter username: ");
+                    userName = Console.ReadLine();
+                    Console.WriteLine("Enter password: ");
+                    userID = Console.ReadLine();
+                    Console.WriteLine("Enter age: ");
+                    inputAge = Console.ReadLine();
+                    Int32.TryParse(inputAge, out userAge);
+                    cmd.CommandText = "INSERT INTO Users(User_ID,User_Name,Age) VALUES(?User_ID,?User_Name,?Age)";
+                    cmd.Parameters.Add("?User_ID", MySqlDbType.VarChar).Value = userID;
+                    cmd.Parameters.Add("?User_Name", MySqlDbType.VarChar).Value = userName;
+                    cmd.Parameters.Add("?Age", MySqlDbType.Int32).Value = userAge;
+                    cmd.ExecuteNonQuery();
+                    Console.WriteLine("Welcome, " + userName + "!");
+                    loggedIn = true;
+                }
+                else if (loginSelect == "2"){ //Login for existing users.
+                    Console.WriteLine("Please enter your username and password.");
+                    Console.WriteLine("Enter username: ");
+                    userName = Console.ReadLine();
+                    Console.WriteLine("Enter password: ");
+                    userID = Console.ReadLine();
+                    Tuple<string, string, string> userCreds = sp1.getUserCred(connString, userID);
+                    bool loginCorrect = false;
+                    while (loginCorrect == false){
+                        if (userID == userCreds.Item1 && userName == userCreds.Item2){
+                            Console.WriteLine("Welcome back, " + userName + "!");
+                            loginCorrect = true;
+                        }
+                        else{
+                            Console.WriteLine("Login failed. Try again.");
+                        }
                     } 
                 }
-
-            }
-            else{
-                Console.WriteLine("Invalid input, try again.");
-                loginSelect = Console.ReadLine();
+                else {
+                    Console.WriteLine("Invalid input, try again.");
+                    loginSelect = Console.ReadLine();
+                }
             }
         
 
             //Questionnaire
+
 
             //Final Output Screen
 
